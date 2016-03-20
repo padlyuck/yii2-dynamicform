08f43ff655f85367cc5febce23db7b005c37bc3f
Vladimir Siritsa <padlyuck@gmail.com>
2016-03-20 23:13:29 +0200

    Kartik datepicker fixed

--- assets/yii2-dynamic-form.js
+++ assets/yii2-dynamic-form.js
@@ -310,11 +310,11 @@
         var widgetOptionsRoot = _getWidgetOptionsRoot(widgetOptions);
 
         // "kartik-v/yii2-widget-datepicker"
-        var $hasDatepicker = $(widgetOptionsRoot.widgetItem).find('[data-krajee-datepicker]');
+        var $hasDatepicker = $(widgetOptionsRoot.widgetItem).find('[data-krajee-kvdatepicker]');
         if ($hasDatepicker.length > 0) {
             $hasDatepicker.each(function() {
-                $(this).parent().removeData().datepicker('remove');
-                $(this).parent().datepicker(eval($(this).attr('data-krajee-datepicker')));
+                $(this).kvDatepicker('remove');
+                $(this).kvDatepicker(eval($(this).attr('data-krajee-kvdatepicker')));
             });
         }
 
